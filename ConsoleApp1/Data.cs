c877e09c88246edda2350204bcf1f01d06207dc7
Alexey Saulin <alexey.saulin03@mail.ru>
2022-06-12 01:11:04 +0300

    Complete dataReader

---
+++ ConsoleApp1/Data.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using DbConnect;
+using Npgsql;
+
+namespace ConsoleApp1;
+
+public class Data
+{
+    private readonly NpgsqlDataReader _dataReader;
+
+    public Data()
+    {
+        var dbConnection = new DBConnection();
+        var npgsqlConnection = dbConnection.NpgsqlConnection;
+        var state = dbConnection.StateTask;
+        while (!state.IsCompleted) {}
+
+        if (npgsqlConnection.State == ConnectionState.Closed)
+            throw new NpgsqlException("Не удалось подключиться к базе данных");
+        const string query = "SELECT * FROM books";
+        var npgsqlCommand = new NpgsqlCommand(query,npgsqlConnection);
+        _dataReader = npgsqlCommand.ExecuteReader();
+    }
+
+    public IEnumerable<NpgsqlDataReader> GetData()
+    {
+        while (_dataReader.Read())
+        {
+            yield return _dataReader;
+        }
+
+        if (_dataReader is {IsClosed: false})
+            _dataReader.Close();
+    }
+}
